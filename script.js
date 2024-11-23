19f29546eef49b1536a739ecb6bf01e7253608d1
maria eloiza <112394213+silvia07102005@users.noreply.github.com>
2024-11-22 22:00:41 -0300

    22

---
+++ script.js
@@ -0,0 +1,4 @@
+// Se desejar adicionar interatividade extra no futuro, como mostrar um modal de vídeo ou fazer animações.
+document.addEventListener('DOMContentLoaded', function() {
+    console.log("Site do Alanzoka carregado!");
+});
